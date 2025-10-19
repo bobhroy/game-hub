2138220422dec5d170838262499f1738996b9c3a
bobhroy <bobhroy@gmail.com>
2025-10-19 16:56:08 -0700

    Clean up the genres and hook correction

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
     id: number;
@@ -24,7 +23,7 @@ const useGames = (
     { 
       params: {
         genres: gameQuery.genre?.id, 
-        platforms: gameQuery.platform?.id,
+        parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText
       }
