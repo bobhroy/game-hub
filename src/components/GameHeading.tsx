1e71d023be108a6900e0322d92a98d60c93ed443
bobhroy <bobhroy@gmail.com>
2025-10-21 13:16:47 -0700

    Create lookup-by-id hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -2,19 +2,17 @@ import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
 import useGenres from '../hooks/useGenres';
 import usePlatforms from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
+import useGenre from '../hooks/useGenre';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
+  const genre = useGenre(gameQuery.genreId);
 
-  const { data: platforms } = usePlatforms();
-  const selectedPlatform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const selectedPlatform = usePlatform(gameQuery.platformId);
 
   const heading = `${selectedPlatform?.name || ''} ${genre?.name || ''} Games`;
 
